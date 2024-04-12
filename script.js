56e4dff7148238817056a5feff047c5f147c6431
meetakhaja <meetbhaiakhaja@gmail.com>
2024-04-12 18:01:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var users = [
+  {
+    profilePic:
+      "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1604681630513-69474a4e253f?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    penddingMessage: 4,
+    location: "Delhi, India",
+    name: "janvi",
+    age: 20,
+    interest: ["coding", "Money Making"],
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quisquam reprehenderit",
+    isfriends: "null",
+  },
+  {
+    profilePic:
+      "https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1502711701722-e02468fc4280?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    penddingMessage: 14,
+    location: "Surat, India",
+    name: "samita",
+    age: 22,
+    interest: ["coding", "Money Making"],
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quisquam reprehenderit",
+    isfriends: "null",
+  },
+  {
+    profilePic:
+      "https://images.unsplash.com/photo-1535737525997-df1c90ca41cb?q=80&w=1366&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1535401249975-b1bfc169ac92?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    penddingMessage: 8,
+    location: "Bhopal, India",
+    name: "rajuli",
+    age: 16,
+    interest: ["coding", "Money Making"],
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quisquam reprehenderit",
+    isfriends: "null",
+  },
+  {
+    profilePic:
+      "https://images.unsplash.com/photo-1536321115970-5dfa13356211?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic:
+      "https://images.unsplash.com/photo-1535419218759-c71f0a6015b3?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    penddingMessage: 23,
+    location: "Bangalore, India",
+    name: "jilu",
+    age: 25,
+    interest: ["coding", "Money Making"],
+    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quisquam reprehenderit",
+    isfriends: "null",
+  },
+];
